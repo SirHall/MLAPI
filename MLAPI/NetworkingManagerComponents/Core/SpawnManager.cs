b5c1b9aeeefaae1df9b4b5216c94a837e00beaa5
SirHall <sirhallstein@gmail.com>
2018-09-11 19:38:27 +1000

    Merged 'NetSpawn' with 'SpawnManager'

--- MLAPI/NetworkingManagerComponents/Core/SpawnManager.cs
+++ MLAPI/NetworkingManagerComponents/Core/SpawnManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using MLAPI.Configuration;
 using MLAPI.Data;
 using MLAPI.Internal;
 using MLAPI.Logging;
@@ -38,6 +39,7 @@ namespace MLAPI.Components
         }
 
         private static NetworkingManager netManager => NetworkingManager.singleton;
+        static NetworkConfig NetworkConfiguration => NetworkingManager.singleton.NetworkConfig;
 
         /// <summary>
         /// Returns the local player object or null if one does not exist
@@ -78,6 +80,54 @@ namespace MLAPI.Components
 
             return ownedObjects.ToArray();
         }
+
+        /// <summary>
+        ///Instantiates a NetworkedPrefab as a NetworkedObject given it's name as a string
+        /// </summary>
+        /// <param name="netPrefabName"></param>
+        /// <param name="pos"></param>
+        /// <param name="rot"></param>
+        /// <returns>The spawned NetworkedObject</returns>
+        public static NetworkedObject Instantiate(string netPrefabName, Vector3? pos = null, Quaternion? rot = null)
+        {
+            if (!NetworkConfiguration.NetworkPrefabNames.ContainsValue(netPrefabName))
+                return null;
+
+            return Instantiate(
+                NetworkConfiguration.NetworkedPrefabs[NetworkConfiguration.NetworkPrefabIds[netPrefabName]],
+                pos,
+                rot
+                );
+        }
+
+        /// <summary>
+        /// Instantiate's a NetworkedObject given it's NetworkedPrefab
+        /// </summary>
+        /// <param name="netPrefab"></param>
+        /// <param name="pos"></param>
+        /// <param name="rot"></param>
+        /// <returns></returns>
+        public static NetworkedObject Instantiate(NetworkedPrefab netPrefab, Vector3? pos = null, Quaternion? rot = null)
+        {
+            //Only do this check on the lowest level method overload
+            if (!NetworkingManager.singleton.isServer)
+                return null;
+
+
+            GameObject obj = netPrefab.prefab;
+
+            obj = GameObject.Instantiate(
+                obj,
+                pos ?? obj.transform.position,
+                rot ?? obj.transform.rotation
+                );
+            NetworkedObject netObj = obj.GetComponent<NetworkedObject>();
+            if (netObj == null) throw new System.Exception($"Network prefab does have a 'NetworkedObject' component on root");
+
+            netObj.Spawn();
+            return netObj;
+        }
+
 
         internal static void RemoveOwnership(uint netId)
         {
